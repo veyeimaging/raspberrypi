0860bac070c0c63e0c57878ebd50b38ec8bea346
Xumm <xumm_td@126.com>
2020-03-15 17:16:04 +0800

    modify hdversion reg addr

--- i2c_cmd/bin/veye_mipi_i2c.sh
+++ i2c_cmd/bin/veye_mipi_i2c.sh
@@ -94,7 +94,7 @@ read_devid()
 {
 	local verid=0;
 	local res=0;
-	res=$(./i2c_read $I2C_DEV $I2C_ADDR  0x00 );
+	res=$(./i2c_read $I2C_DEV $I2C_ADDR  0x01 );
 	verid=$?;
 	printf "device id is 0x%2x\n" $verid;
 }
@@ -103,7 +103,7 @@ read_hardver()
 {
 	local hardver=0;
 	local res=0;
-	res=$(./i2c_read $I2C_DEV $I2C_ADDR  0x01 );
+	res=$(./i2c_read $I2C_DEV $I2C_ADDR  0x00 );
 	hardver=$?;
 	printf "hardware version is 0x%2x\n" $hardver;
 }
